e000c7ffb5a7e0101427e1fef4bcef582208fb29
Malik MONDESIR <malikmondesir@gmail.com>
2021-07-28 18:56:01 +0200

    fix: edit not resgistered link in /login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
 
             <div class="flex items-center justify-end mt-4">
 
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('auth.login') }}">
                     {{ __('Not registered?') }}
                 </a>
             </div>
